33bdeaf865ef6f4db2f284dd68604c2864093cef
Samarth Jaiswal <samjaiswal51@gmail.com>
2025-08-20 19:11:13 +0530

    create new api

--- index.js
+++ index.js
@@ -83,3 +83,9 @@ app.get("/posts/:id/edit",(req,res)=>{
     let post = posts.find((p)=>p.id === id);
     res.render("edit.ejs", {post});
 })
+
+app.delete("/posts/:id",(req,res)=>{
+    let {id} = req.params;
+    posts = posts.filter((p)=>id !== p.id);
+    res.redirect("/posts");
+})
