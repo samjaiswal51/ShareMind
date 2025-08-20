6bb2b3d44d391644c1ea91a1c37c3dc4a9cf8f0a
Samarth Jaiswal <samjaiswal51@gmail.com>
2025-08-20 15:39:34 +0530

    create new api

--- index.js
+++ index.js
@@ -41,3 +41,14 @@ let posts = [
 app.get("/posts",(req,res)=>{
     res.render("index.ejs", { posts});
 })
+
+app.get("/posts/new",(req,res)=>{
+    res.render("new.ejs");
+});
+
+app.post("/posts",(req,res)=>{
+    let {username, content} = req.body;
+    posts.push({username,content});
+
+    res.redirect("/posts");
+})
